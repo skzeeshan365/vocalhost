63ac5ce00c3ac8cb0c27221a679cc2cc35294899
zeeshan <skzeeshan3650@gmail.com>
2023-06-26 19:34:53 +0530

    v1.1

--- ReiserX_Tunnel/asgi.py
+++ ReiserX_Tunnel/asgi.py
@@ -1,11 +1,12 @@
 import os
-
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-
-from main.routing import websocket_urlpatterns
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ReiserX_Tunnel.settings')
+django.setup()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from main.routing import websocket_urlpatterns
+from django.core.asgi import get_asgi_application
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
--- ReiserX_Tunnel/settings.py
+++ ReiserX_Tunnel/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "channels",
-    "main.apps.MainConfig",
+    "main.apps.MainConfig"
 ]
 
 MIDDLEWARE = [
