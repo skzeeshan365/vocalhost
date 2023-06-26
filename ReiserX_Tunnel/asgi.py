94e82b59d77dbaaefceec9cbcefa30729de01774
zeeshan <skzeeshan3650@gmail.com>
2023-06-26 19:01:42 +0530

    v1.1

--- ReiserX_Tunnel/asgi.py
+++ ReiserX_Tunnel/asgi.py
@@ -2,7 +2,6 @@ import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from main.routing import websocket_urlpatterns
-from .ChannelAuthMiddleware import AuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ReiserX_Tunnel.settings')
 
